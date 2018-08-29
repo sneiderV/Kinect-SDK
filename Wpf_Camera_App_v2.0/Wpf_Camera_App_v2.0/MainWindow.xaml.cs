5e59720f4f15d583e2beb87531f478a938d5ef23
Sneider Velandia Gonzalez <e.velandia2164@uniandes.edu.co>
2018-08-28 20:29:06 -0500

    Wpf camera app /

---
+++ Wpf_Camera_App_v2.0/Wpf_Camera_App_v2.0/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+
+namespace Wpf_Camera_App_v2._0
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        KinectSensor miKinect = null;
+        ColorFrameReader colorFrameReader = null;
+        WriteableBitmap colorBitmap = null;
+
+
+        public MainWindow()
+        {
+            miKinect = KinectSensor.GetDefault();
+            if(miKinect != null)
+            {
+                miKinect.Open();
+            }
+            else
+            {
+                MessageBox.Show("El kinect no se reconoce");
+            }
+
+            colorFrameReader = miKinect.ColorFrameSource.OpenReader();
+            if(colorFrameReader != null)
+            {
+                colorFrameReader.FrameArrived += ColorFrameReader_FrameArrived;
+            }
+
+            FrameDescription colorFrameDescription = miKinect.ColorFrameSource.CreateFrameDescription(ColorImageFormat.Bgra);
+            colorBitmap = new WriteableBitmap(colorFrameDescription.Width, colorFrameDescription.Height, 96.0, 96.0, PixelFormats.Bgr32, null);
+
+            InitializeComponent();
+        }
+
+        private void ColorFrameReader_FrameArrived(object sender, ColorFrameArrivedEventArgs e)
+        {
+            using (ColorFrame colorFrame = e.FrameReference.AcquireFrame())
+            {
+                if (colorFrame != null)
+                {
+                    FrameDescription colorFrameDescription = colorFrame.FrameDescription;
+
+                    using (KinectBuffer colorBuffer = colorFrame.LockRawImageBuffer())
+                    {
+                        colorBitmap.Lock();
+
+                        // verify data and write the new color frame data to the display bitmap
+                        if ((colorFrameDescription.Width == colorBitmap.PixelWidth) && (colorFrameDescription.Height == this.colorBitmap.PixelHeight))
+                        {
+                            colorFrame.CopyConvertedFrameDataToIntPtr(
+                                this.colorBitmap.BackBuffer,
+                                (uint)(colorFrameDescription.Width * colorFrameDescription.Height * 4),
+                                ColorImageFormat.Bgra);
+
+                            this.colorBitmap.AddDirtyRect(new Int32Rect(0, 0, this.colorBitmap.PixelWidth, this.colorBitmap.PixelHeight));
+                        }
+
+                        this.colorBitmap.Unlock();
+                    }
+                }
+
+            }
+        }
+    }
+}
