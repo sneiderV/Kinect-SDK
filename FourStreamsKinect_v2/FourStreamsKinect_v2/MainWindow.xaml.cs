f2d84536714e8e92a99f2e98a68ae1edf3d979b4
Sneider Velandia Gonzalez <e.velandia2164@uniandes.edu.co>
2018-09-04 18:13:25 -0500

    Four streams kinect
    This app have 4 of the streams frames that provide the sensors of kinect v2

---
+++ FourStreamsKinect_v2/FourStreamsKinect_v2/MainWindow.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+
+namespace FourStreamsKinect_v2
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        KinectSensor sKinect;
+        MultiSourceFrameReader sReader; 
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //asignamos el kinect que este conectado
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            sKinect = KinectSensor.GetDefault();
+
+            if (sKinect != null)
+            {
+                sKinect.Open();
+                sReader = sKinect.OpenMultiSourceFrameReader(FrameSourceTypes.Color | FrameSourceTypes.Depth | FrameSourceTypes.Infrared | FrameSourceTypes.Body);
+                sReader.MultiSourceFrameArrived += SReader_MultiSourceFrameArrived; //se usa para cada frame que llega
+            }
+            else
+            {
+                MessageBox.Show("No se reconoce un kinect");
+            }
+
+        }
+
+        private void SReader_MultiSourceFrameArrived(object sender, MultiSourceFrameArrivedEventArgs e)
+        {
+            //obtener la referencia del multi-frame
+            var reference = e.FrameReference.AcquireFrame();
+
+            //abrir el frame de color
+            using (var frame = reference.ColorFrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+
+                }
+            }
+
+            //abrir el frame de profundidad
+            using (var frame = reference.DepthFrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+
+                }
+            }
+
+            //abrir el frame de infrarrojo
+            using (var frame = reference.InfraredFrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+
+                }
+            }
+
+            //abrir el frame del cuerpo
+            using (var frame = reference.BodyFrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+
+                }
+            }
+        }
+
+        private void Body_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Infrared_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Depth_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Camera_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+       
+    }
+}
---
+++ FourStreamsKinect_v2/FourStreamsKinect_v2/ReadersFrames.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+
+namespace FourStreamsKinect_v2
+{
+    public static class ReadersFrames
+    {
+        public static ImageSource ToBitmap(ColorFrame frame)
+        {
+            int width = frame.FrameDescription.Width;
+            int height = frame.FrameDescription.Height;
+            PixelFormat format = PixelFormats.Bgr32;
+
+            byte[] pixels = new byte[width * height * ((PixelFormats.Bgr32.BitsPerPixel + 7) / 8)];
+
+            if (frame.RawColorImageFormat == ColorImageFormat.Bgra)
+            {
+                frame.CopyRawFrameDataToArray(pixels);
+            }
+            else
+            {
+                frame.CopyConvertedFrameDataToArray(pixels, ColorImageFormat.Bgra);
+            }
+
+            int stride = width * format.BitsPerPixel / 8;
+
+            return BitmapSource.Create(width, height, 96, 96, format, null, pixels, stride);
+        }
+    }
+}
