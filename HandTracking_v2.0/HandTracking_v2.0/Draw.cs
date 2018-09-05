4d5a84c21be54cae4bc4b6493ae84823f653f01f
Sneider Velandia Gonzalez <e.velandia2164@uniandes.edu.co>
2018-09-04 22:09:18 -0500

    Hand tracking
    This application allows you to recognize the gestures of the hands, these are: open, close or not recognized.

---
+++ HandTracking_v2.0/HandTracking_v2.0/Draw.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+
+
+
+namespace HandTracking_v2._0
+{
+    //
+    public static class Draw
+    {
+
+        public static Point Scale(this Joint joint, CoordinateMapper mapper)
+        {
+            Point point = new Point();
+
+            ColorSpacePoint colorPoint = mapper.MapCameraPointToColorSpace(joint.Position);
+            point.X = float.IsInfinity(colorPoint.X) ? 0.0 : colorPoint.X;
+            point.Y = float.IsInfinity(colorPoint.Y) ? 0.0 : colorPoint.Y;
+
+            return point;
+        }
+
+        //Color stream
+        public static ImageSource ToBitmap(ColorFrame frame)
+        {
+            int width = frame.FrameDescription.Width;
+            int height = frame.FrameDescription.Height;
+            PixelFormat format = PixelFormats.Bgr32;
+
+            byte[] pixels = new byte[width * height * ((PixelFormats.Bgr32.BitsPerPixel + 7) / 8)];
+
+            if (frame.RawColorImageFormat == ColorImageFormat.Bgra)
+            {
+                frame.CopyRawFrameDataToArray(pixels);
+            }
+            else
+            {
+                frame.CopyConvertedFrameDataToArray(pixels, ColorImageFormat.Bgra);
+            }
+
+            int stride = width * format.BitsPerPixel / 8;
+
+            return BitmapSource.Create(width, height, 96, 96, format, null, pixels, stride);
+        }
+        
+        //dibujar mano
+        public static void DrawHand(this Canvas canvas, Joint hand, CoordinateMapper mapper)
+        {
+            if (hand.TrackingState == TrackingState.NotTracked) return;
+
+            Point point = hand.Scale(mapper);
+
+            Ellipse ellipse = new Ellipse
+            {
+                Width = 100,
+                Height = 100,
+                Stroke = new SolidColorBrush(Colors.LightBlue),
+                StrokeThickness = 4
+            };
+
+            Canvas.SetLeft(ellipse, point.X - ellipse.Width / 2);
+            Canvas.SetTop(ellipse, point.Y - ellipse.Height / 2);
+
+            canvas.Children.Add(ellipse);
+        }
+
+    }
+}
---
+++ HandTracking_v2.0/HandTracking_v2.0/MainWindow.xaml.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+
+namespace HandTracking_v2._0
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        KinectSensor sKinect;
+        MultiSourceFrameReader sReader;
+        IList<Body> bodies;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //asignamos el kinect que este conectado
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            sKinect = KinectSensor.GetDefault();
+
+            if (sKinect != null)
+            {
+                sKinect.Open();
+                sReader = sKinect.OpenMultiSourceFrameReader(FrameSourceTypes.Color | FrameSourceTypes.Depth | FrameSourceTypes.Infrared | FrameSourceTypes.Body);
+                sReader.MultiSourceFrameArrived += SReader_MultiSourceFrameArrived; //se usa para cada frame que llega
+            }
+            else
+            {
+                MessageBox.Show("No se reconoce un kinect");
+            }
+
+        }
+
+        private void SReader_MultiSourceFrameArrived(object sender, MultiSourceFrameArrivedEventArgs e)
+        {
+            //obtener la referencia del multi-frame
+            var reference = e.FrameReference.AcquireFrame();
+
+            //abrir el frame de color
+            using (var frame = reference.ColorFrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                   i_camera.Source = Draw.ToBitmap(frame);
+                }
+            }
+
+            //abir el frame de body 
+            using (var frame = reference.BodyFrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    c_body.Children.Clear();
+
+                    bodies = new Body[frame.BodyFrameSource.BodyCount];
+
+                    frame.GetAndRefreshBodyData(bodies);
+
+                    foreach (var body in bodies)
+                    {
+                        if (body != null)
+                        {
+                            if (body.IsTracked)
+                            {
+                                // Find the joints
+                                Joint handRight = body.Joints[JointType.HandRight];
+                                Joint thumbRight = body.Joints[JointType.ThumbRight];
+
+                                Joint handLeft = body.Joints[JointType.HandLeft];
+                                Joint thumbLeft = body.Joints[JointType.ThumbLeft];
+
+                                // Draw hands and thumbs
+                                c_body.DrawHand(handRight, sKinect.CoordinateMapper);
+                                c_body.DrawHand(handLeft, sKinect.CoordinateMapper);
+                            //  c_body.DrawThumb(thumbRight, _sensor.CoordinateMapper);
+                            //  c_body.DrawThumb(thumbLeft, _sensor.CoordinateMapper);
+
+                                // Find the hand states
+                                string rightHandState = "-";
+                                string leftHandState = "-";
+
+                                switch (body.HandRightState)
+                                {
+                                    case HandState.Open:
+                                        rightHandState = "Open";
+                                        break;
+                                    case HandState.Closed:
+                                        rightHandState = "Closed";
+                                        break;
+                                    case HandState.Lasso:
+                                        rightHandState = "Lasso";
+                                        break;
+                                    case HandState.Unknown:
+                                        rightHandState = "Unknown...";
+                                        break;
+                                    case HandState.NotTracked:
+                                        rightHandState = "Not tracked";
+                                        break;
+                                    default:
+                                        break;
+                                }
+
+                                switch (body.HandLeftState)
+                                {
+                                    case HandState.Open:
+                                        leftHandState = "Open";
+                                        break;
+                                    case HandState.Closed:
+                                        leftHandState = "Closed";
+                                        break;
+                                    case HandState.Lasso:
+                                        leftHandState = "Lasso";
+                                        break;
+                                    case HandState.Unknown:
+                                        leftHandState = "Unknown...";
+                                        break;
+                                    case HandState.NotTracked:
+                                        leftHandState = "Not tracked";
+                                        break;
+                                    default:
+                                        break;
+                                }
+
+                                tcRightHandState.Text = rightHandState;
+                                tcLeftHandState.Text = leftHandState;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
